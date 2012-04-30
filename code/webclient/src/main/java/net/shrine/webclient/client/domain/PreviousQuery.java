0de602b8c5816d9967443818bde07537cfe82b41
cgilbert <cgilbert@c2d9aaf4-0be0-4a0d-89e0-85a3af7b581e>
2012-04-30 20:07:42 +0000

    Added PreviousQuery DTO

---
+++ code/webclient/src/main/java/net/shrine/webclient/client/domain/PreviousQuery.java
@@ -0,0 +1,86 @@
+package net.shrine.webclient.client.domain;
+
+import java.util.Date;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * 
+ * @author clint
+ * @date Apr 27, 2012
+ */
+public final class PreviousQuery implements IsSerializable, Comparable<PreviousQuery> {
+
+	private final String queryId;
+
+	private final Date date;
+
+	//NB: For GWT Serialization
+	@SuppressWarnings("unused")
+	private PreviousQuery() {
+		this(null, null);
+	}
+	
+	public PreviousQuery(final String queryId, final Date date) {
+		super();
+
+		this.queryId = queryId;
+		this.date = date;
+	}
+	
+	@Override
+	public int compareTo(final PreviousQuery o) {
+		return this.date.compareTo(o.date);
+	}
+
+	public String getQueryId() {
+		return queryId;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (date == null ? 0 : date.hashCode());
+		result = prime * result + (queryId == null ? 0 : queryId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final PreviousQuery other = (PreviousQuery) obj;
+		if (date == null) {
+			if (other.date != null) {
+				return false;
+			}
+		} else if (!date.equals(other.date)) {
+			return false;
+		}
+		if (queryId == null) {
+			if (other.queryId != null) {
+				return false;
+			}
+		} else if (!queryId.equals(other.queryId)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "PreviousQuery [" + queryId + ", " + date + "]";
+	}
+}
