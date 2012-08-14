2e2cf4978b5dc6f5442a6bf8e77276cdc1316e07
bsimons <bsimons@c2d9aaf4-0be0-4a0d-89e0-85a3af7b581e>
2012-08-14 14:51:25 +0000

    validate user response from PM authentication

--- code/webclient/src/main/scala/net/shrine/webclient/server/PMAuthenticationProvider.scala
+++ code/webclient/src/main/scala/net/shrine/webclient/server/PMAuthenticationProvider.scala
@@ -1,6 +1,6 @@
 package net.shrine.webclient.server
 
-import org.springframework.security.authentication.{UsernamePasswordAuthenticationToken, AuthenticationProvider}
+import org.springframework.security.authentication.{BadCredentialsException, UsernamePasswordAuthenticationToken, AuthenticationProvider}
 import org.springframework.security.core.Authentication
 import net.shrine.i2b2.protocol.pm.{User, HiveConfig, GetUserConfigurationRequest}
 import org.springframework.security.core.authority.{SimpleGrantedAuthority, GrantedAuthorityImpl}
@@ -17,18 +17,25 @@ import net.shrine.util.HTTPClient
  * @link http://www.gnu.org/licenses/lgpl.html
  */
 final class PMAuthenticationProvider(val pmEndpoint: String) extends AuthenticationProvider {
+
+  def validUser(token: DomainUsernamePasswordAuthenticationToken, user: User): Boolean = (token.domain == user.domain) && (token.principal == user.username)
+
   def authenticate(authentication: Authentication) = {
-    val domain = authentication.asInstanceOf[DomainUsernamePasswordAuthenticationToken].domain
-    val pmRequest = new GetUserConfigurationRequest(domain, authentication.getPrincipal.asInstanceOf[String], authentication.getCredentials.asInstanceOf[String])
+    val token = authentication.asInstanceOf[DomainUsernamePasswordAuthenticationToken]
+    val domain = token.domain
+    val pmRequest = new GetUserConfigurationRequest(domain, token.principal, authentication.getCredentials.asInstanceOf[String])
 
     val responseXml: String = HTTPClient.post(pmRequest.toI2b2String, pmEndpoint)
-    val user = User.fromI2b2(responseXml) //TODO need error checking in parser
+    val user = User.fromI2b2(responseXml)
 
-    //TODO validate user
+    if(!validUser(token, user)) {
+      throw new BadCredentialsException("Invalid credentials")
+    }
 
     import scala.collection.JavaConverters._
-    
+    //TODO - parse Security information from user response and include i2b2 password (isToken = true) in authentication token
     new UsernamePasswordAuthenticationToken(authentication.getPrincipal, authentication.getCredentials, Seq(new SimpleGrantedAuthority("ROLE_USER")).asJava)
+
   }
 
   def supports(authenticationClass: Class[_]) = classOf[DomainUsernamePasswordAuthenticationToken].isAssignableFrom(authenticationClass)
