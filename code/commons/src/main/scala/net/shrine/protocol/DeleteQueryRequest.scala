4941bfdf89f43178eb68241f4c5d90ffbafcd4d4
cgilbert <cgilbert@c2d9aaf4-0be0-4a0d-89e0-85a3af7b581e>
2012-08-15 22:12:51 +0000

    Finished factoring out i2b2 PSM header building to new CrcRequest trait

--- code/commons/src/main/scala/net/shrine/protocol/DeleteQueryRequest.scala
+++ code/commons/src/main/scala/net/shrine/protocol/DeleteQueryRequest.scala
@@ -14,21 +14,21 @@ import net.shrine.serialization.I2b2Unmarshaller
  *       NOTICE: This software comes with NO guarantees whatsoever and is
  *       licensed as Lgpl Open Source
  * @link http://www.gnu.org/licenses/lgpl.html
- * 
+ *
  * NB: this is a case class to get a structural equality contract in hashCode and equals, mostly for testing
  */
 final case class DeleteQueryRequest(
-    override val projectId: String,
-    override val waitTimeMs: Long,
-    override val authn: AuthenticationInfo,
-    val queryId: Long) extends ShrineRequest(projectId, waitTimeMs, authn) with TranslatableRequest[DeleteQueryRequest] {
+  override val projectId: String,
+  override val waitTimeMs: Long,
+  override val authn: AuthenticationInfo,
+  val queryId: Long) extends ShrineRequest(projectId, waitTimeMs, authn) with CrcRequest with TranslatableRequest[DeleteQueryRequest] {
 
   val requestType = MasterDeleteRequestType
 
   def toXml = XmlUtil.stripWhitespace(
     <deleteQuery>
-      {headerFragment}
-      <queryId>{queryId}</queryId>
+      { headerFragment }
+      <queryId>{ queryId }</queryId>
     </deleteQuery>)
 
   def handle(handler: ShrineRequestHandler) = {
@@ -43,16 +43,11 @@ final case class DeleteQueryRequest(
 
   protected def i2b2MessageBody = XmlUtil.stripWhitespace(
     <message_body>
-      <ns4:psmheader>
-            <user login={authn.username}>{authn.username}</user>
-            <patient_set_limit>0</patient_set_limit>
-            <estimated_time>0</estimated_time>
-            <request_type>CRC_QRY_deleteQueryMaster</request_type>
-          </ns4:psmheader>
-          <ns4:request xsi:type="ns4:master_delete_requestType" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            <user_id>{authn.username}</user_id>
-            <query_master_id>{queryId}</query_master_id>
-          </ns4:request>
+      { i2b2PsmHeader }
+      <ns4:request xsi:type="ns4:master_delete_requestType" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <user_id>{ authn.username }</user_id>
+        <query_master_id>{ queryId }</query_master_id>
+      </ns4:request>
     </message_body>)
 }
 
--- code/commons/src/main/scala/net/shrine/protocol/ReadInstanceResultsRequest.scala
+++ code/commons/src/main/scala/net/shrine/protocol/ReadInstanceResultsRequest.scala
@@ -5,7 +5,6 @@ import net.shrine.protocol.CRCRequestType.InstanceRequestType
 import net.shrine.util.XmlUtil
 import net.shrine.serialization.I2b2Unmarshaller
 
-
 /**
  * @author Bill Simons
  * @date 3/17/11
@@ -15,21 +14,21 @@ import net.shrine.serialization.I2b2Unmarshaller
  *       NOTICE: This software comes with NO guarantees whatsoever and is
  *       licensed as Lgpl Open Source
  * @link http://www.gnu.org/licenses/lgpl.html
- * 
+ *
  * NB: this is a case class to get a structural equality contract in hashCode and equals, mostly for testing
  */
 final case class ReadInstanceResultsRequest(
-    override val projectId: String,
-    override val waitTimeMs: Long,
-    override val authn: AuthenticationInfo,
-    val instanceId: Long) extends ShrineRequest(projectId, waitTimeMs, authn) with TranslatableRequest[ReadInstanceResultsRequest] {
+  override val projectId: String,
+  override val waitTimeMs: Long,
+  override val authn: AuthenticationInfo,
+  val instanceId: Long) extends ShrineRequest(projectId, waitTimeMs, authn) with CrcRequest with TranslatableRequest[ReadInstanceResultsRequest] {
 
   val requestType = InstanceRequestType
 
   def toXml = XmlUtil.stripWhitespace(
     <readInstanceResults>
-      {headerFragment}
-      <instanceId>{instanceId}</instanceId>
+      { headerFragment }
+      <instanceId>{ instanceId }</instanceId>
     </readInstanceResults>)
 
   def handle(handler: ShrineRequestHandler) = {
@@ -44,15 +43,10 @@ final case class ReadInstanceResultsRequest(
 
   protected def i2b2MessageBody = XmlUtil.stripWhitespace(
     <message_body>
-      <ns4:psmheader>
-        <user login={authn.username}>{authn.username}</user>
-        <patient_set_limit>0</patient_set_limit>
-        <estimated_time>0</estimated_time>
-        <request_type>CRC_QRY_getQueryResultInstanceList_fromQueryInstanceId</request_type>
-      </ns4:psmheader>
-        <ns4:request xsi:type="ns4:instance_requestType" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-          <query_instance_id>{instanceId}</query_instance_id>
-        </ns4:request>
+      { i2b2PsmHeader }
+      <ns4:request xsi:type="ns4:instance_requestType" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <query_instance_id>{ instanceId }</query_instance_id>
+      </ns4:request>
     </message_body>)
 }
 
