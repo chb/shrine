658f88a3d755d6e2d3321247bc19547ccf2f36d8
cgilbert <cgilbert@c2d9aaf4-0be0-4a0d-89e0-85a3af7b581e>
2012-08-16 01:12:15 +0000

    Added class to represent an <i2b2_result_envelope>; only int columns are supported for now

---
+++ code/commons/src/main/scala/net/shrine/protocol/I2b2ResultEnvelope.scala
@@ -0,0 +1,91 @@
+package net.shrine.protocol
+
+import scala.xml.NodeSeq
+import I2b2ResultEnvelope.Column
+import I2b2ResultEnvelope._
+import com.sun.org.apache.xalan.internal.xsltc.compiler.ValueOf
+import scala.xml.XML
+
+/**
+ * @author clint
+ * @date Aug 15, 2012
+ */
+final case class I2b2ResultEnvelope(resultType: ResultOutputType, columns: Seq[Column[_]]) {
+  def +[T](column: Column[T]): I2b2ResultEnvelope = this.copy(columns = columns :+ column)
+
+  def +[T: I2b2ColumnType](column: (String, T)): I2b2ResultEnvelope = {
+    val (name, value) = column
+
+    val columnType = implicitly[I2b2ColumnType[T]].toI2b2
+    
+    this + Column(columnType, name, value)
+  }
+
+  //TODO: Produce gross i2b2 semi-escaped form?
+  def toI2b2: NodeSeq = {
+    <ns10:i2b2_result_envelope xmlns:ns2="http://www.i2b2.org/xsd/hive/pdo/1.1/" xmlns:ns4="http://www.i2b2.org/xsd/cell/crc/psm/1.1/" xmlns:ns3="http://www.i2b2.org/xsd/cell/crc/pdo/1.1/" xmlns:ns9="http://www.i2b2.org/xsd/cell/ont/1.1/" xmlns:ns5="http://www.i2b2.org/xsd/hive/msg/1.1/" xmlns:ns6="http://www.i2b2.org/xsd/cell/crc/psm/querydefinition/1.1/" xmlns:ns10="http://www.i2b2.org/xsd/hive/msg/result/1.1/" xmlns:ns7="http://www.i2b2.org/xsd/cell/crc/psm/analysisdefinition/1.1/" xmlns:ns8="http://www.i2b2.org/xsd/cell/pm/1.1/">
+      <body>
+        <ns10:result name={ resultType.name }>
+          { columns.map(_.toI2b2) }
+        </ns10:result>
+      </body>
+    </ns10:i2b2_result_envelope>
+  }
+}
+
+object I2b2ResultEnvelope {
+  sealed trait I2b2ColumnType[T] {
+    def toI2b2: String
+    
+    def fromI2b2(s: String): T
+  }
+
+  object I2b2ColumnType {
+    implicit object intI2b2ColumnType extends I2b2ColumnType[Int] {
+      override def toI2b2 = "int"
+        
+      override def fromI2b2(serialized: String): Int = serialized.toInt
+    }
+    
+    def fromI2b2(columnTypeName: String): Option[I2b2ColumnType[_]] = {
+      columnTypeName match {
+        case "int" => Some(intI2b2ColumnType)
+        case _ => None
+      }
+    }
+  }
+
+  final case class Column[T](columnType: String, name: String, value: T) {
+    def toI2b2: NodeSeq = {
+      <data type={ columnType } column={ name }>{ value }</data>
+    }
+  }
+
+  def empty(resultType: ResultOutputType) = new I2b2ResultEnvelope(resultType, Seq.empty)
+  
+  def fromI2b2String(i2b2Xml: String): Option[I2b2ResultEnvelope] = fromI2b2(XML.loadString(I2b2Workarounds.unescape(i2b2Xml)))
+  
+  def fromI2b2(i2b2Xml: NodeSeq): Option[I2b2ResultEnvelope] = {
+    
+    val resultBody = i2b2Xml \ "body" \ "result"
+    
+    val resultTypeOption = tryOrNone(ResultOutputType.valueOf((resultBody \ "@name").text))
+    
+    val columns = for {
+      columnXml <- resultBody \ "data"
+      columnType <- I2b2ColumnType.fromI2b2((columnXml \ "@type").text)
+      name = (columnXml \ "@column").text
+      value = columnXml.text
+    } yield Column(columnType.toI2b2, name, columnType.fromI2b2(value))
+
+    resultTypeOption.map(I2b2ResultEnvelope(_, columns))
+  }
+  
+  private def tryOrNone[T](f: => T): Option[T] = {
+    try {
+      Option(f)
+    } catch {
+      case e: Exception => None
+    }
+  }
+}
---
+++ code/commons/src/test/scala/net/shrine/protocol/I2b2ResultEnvelopeTest.scala
@@ -0,0 +1,147 @@
+package net.shrine.protocol
+
+import junit.framework.TestCase
+import org.scalatest.junit.AssertionsForJUnit
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.Test
+import net.shrine.protocol.I2b2ResultEnvelope.Column
+
+/**
+ * @author clint
+ * @date Aug 15, 2012
+ */
+final class I2b2ResultEnvelopeTest extends TestCase with AssertionsForJUnit with ShouldMatchers {
+  @Test
+  def testFromI2b2 {
+    val xml = """&lt;?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+&lt;ns10:i2b2_result_envelope xmlns:ns2="http://www.i2b2.org/xsd/hive/pdo/1.1/" xmlns:ns4="http://www.i2b2.org/xsd/cell/crc/psm/1.1/" xmlns:ns3="http://www.i2b2.org/xsd/cell/crc/pdo/1.1/" xmlns:ns9="http://www.i2b2.org/xsd/cell/ont/1.1/" xmlns:ns5="http://www.i2b2.org/xsd/hive/msg/1.1/" xmlns:ns6="http://www.i2b2.org/xsd/cell/crc/psm/querydefinition/1.1/" xmlns:ns10="http://www.i2b2.org/xsd/hive/msg/result/1.1/" xmlns:ns7="http://www.i2b2.org/xsd/cell/crc/psm/analysisdefinition/1.1/" xmlns:ns8="http://www.i2b2.org/xsd/cell/pm/1.1/">
+    &lt;body>
+        &lt;ns10:result name="PATIENT_AGE_COUNT_XML">
+            &lt;data type="int" column="  0-9 years old">0&lt;/data>
+            &lt;data type="int" column="  10-17 years old">11&lt;/data>
+            &lt;data type="int" column="  18-34 years old">26&lt;/data>
+            &lt;data type="int" column="  35-44 years old">26&lt;/data>
+            &lt;data type="int" column="  45-54 years old">8&lt;/data>
+            &lt;data type="int" column="  55-64 years old">6&lt;/data>
+            &lt;data type="int" column="  65-74 years old">5&lt;/data>
+            &lt;data type="int" column="  75-84 years old">0&lt;/data>
+            &lt;data type="int" column="&amp;gt;= 65 years old">5&lt;/data>
+            &lt;data type="int" column="&amp;gt;= 85 years old">0&lt;/data>
+            &lt;data type="int" column="Not recorded">0&lt;/data>
+        &lt;/ns10:result>
+    &lt;/body>
+&lt;/ns10:i2b2_result_envelope>"""
+
+    val env = I2b2ResultEnvelope.fromI2b2String(xml).get
+    
+    env.resultType should be(ResultOutputType.PATIENT_AGE_COUNT_XML)
+    
+    def column(name: String, value: Int) = Column("int", name, value)
+    
+    val expected = Seq(column("  0-9 years old", 0),
+                       column("  10-17 years old", 11),
+                       column("  18-34 years old", 26),
+                       column("  35-44 years old", 26),
+                       column("  45-54 years old", 8),
+                       column("  55-64 years old", 6),
+                       column("  65-74 years old", 5),
+                       column("  75-84 years old", 0),
+                       column(">= 65 years old", 5),
+                       column(">= 85 years old", 0),
+                       column("Not recorded", 0))
+                       
+    env.columns.zip(expected).foreach { case (actual, expected) =>
+      actual.columnType should equal(expected.columnType)
+      actual.name should equal(expected.name)
+      actual.value should equal(expected.value)
+    }
+    
+    val badXml1 = """&lt;?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+&lt;ns10:i2b2_result_envelope xmlns:ns2="http://www.i2b2.org/xsd/hive/pdo/1.1/" xmlns:ns4="http://www.i2b2.org/xsd/cell/crc/psm/1.1/" xmlns:ns3="http://www.i2b2.org/xsd/cell/crc/pdo/1.1/" xmlns:ns9="http://www.i2b2.org/xsd/cell/ont/1.1/" xmlns:ns5="http://www.i2b2.org/xsd/hive/msg/1.1/" xmlns:ns6="http://www.i2b2.org/xsd/cell/crc/psm/querydefinition/1.1/" xmlns:ns10="http://www.i2b2.org/xsd/hive/msg/result/1.1/" xmlns:ns7="http://www.i2b2.org/xsd/cell/crc/psm/analysisdefinition/1.1/" xmlns:ns8="http://www.i2b2.org/xsd/cell/pm/1.1/">
+    &lt;body>
+        &lt;ns10:result name="PATIENT_AGE_COUNT_XML_JKALSHFKAJSFHKASHFJKSAHFKHJH">
+            &lt;data type="int" column="  0-9 years old">0&lt;/data>
+            &lt;data type="int" column="  10-17 years old">11&lt;/data>
+            &lt;data type="int" column="  18-34 years old">26&lt;/data>
+            &lt;data type="int" column="  35-44 years old">26&lt;/data>
+            &lt;data type="int" column="  45-54 years old">8&lt;/data>
+            &lt;data type="int" column="  55-64 years old">6&lt;/data>
+            &lt;data type="int" column="  65-74 years old">5&lt;/data>
+            &lt;data type="int" column="  75-84 years old">0&lt;/data>
+            &lt;data type="int" column="&amp;gt;= 65 years old">5&lt;/data>
+            &lt;data type="int" column="&amp;gt;= 85 years old">0&lt;/data>
+            &lt;data type="int" column="Not recorded">0&lt;/data>
+        &lt;/ns10:result>
+    &lt;/body>
+&lt;/ns10:i2b2_result_envelope>"""
+      
+      I2b2ResultEnvelope.fromI2b2String(badXml1) should be(None)
+    
+     val badXml2 = """&lt;?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+&lt;ns10:i2b2_result_envelope xmlns:ns2="http://www.i2b2.org/xsd/hive/pdo/1.1/" xmlns:ns4="http://www.i2b2.org/xsd/cell/crc/psm/1.1/" xmlns:ns3="http://www.i2b2.org/xsd/cell/crc/pdo/1.1/" xmlns:ns9="http://www.i2b2.org/xsd/cell/ont/1.1/" xmlns:ns5="http://www.i2b2.org/xsd/hive/msg/1.1/" xmlns:ns6="http://www.i2b2.org/xsd/cell/crc/psm/querydefinition/1.1/" xmlns:ns10="http://www.i2b2.org/xsd/hive/msg/result/1.1/" xmlns:ns7="http://www.i2b2.org/xsd/cell/crc/psm/analysisdefinition/1.1/" xmlns:ns8="http://www.i2b2.org/xsd/cell/pm/1.1/">
+    &lt;body>
+        &lt;ns10:result name="PATIENT_AGE_COUNT_XML">
+            &lt;foo type="int" column="  0-9 years old">0&lt;/foo>
+        &lt;/ns10:result>
+    &lt;/body>
+&lt;/ns10:i2b2_result_envelope>"""
+       
+     I2b2ResultEnvelope.fromI2b2String(badXml2).get.columns should equal(Nil)
+  }
+
+  @Test
+  def testPlusColumn {
+    val resultType = ResultOutputType.PATIENT_COUNT_XML
+
+    val empty = I2b2ResultEnvelope.empty(resultType)
+
+    val column1 = Column("int", "foo", 123)
+
+    val env1 = empty + column1
+
+    (env1 eq empty) should be(false)
+
+    env1.resultType should be(resultType)
+
+    env1.columns should equal(Seq(column1))
+
+    val column2 = Column("int", "nuh", 123)
+
+    val env2 = env1 + column2
+
+    (env2 eq env1) should be(false)
+
+    env2.resultType should be(resultType)
+    env2.columns should equal(Seq(column1, column2))
+  }
+
+  @Test
+  def testPlusTuple {
+    val resultType = ResultOutputType.PATIENT_COUNT_XML
+
+    val empty = I2b2ResultEnvelope.empty(resultType)
+
+    val env = empty + ("foo" -> 123)
+
+    (env eq empty) should be(false)
+
+    env.resultType should be(resultType)
+
+    env.columns should equal(Seq(Column("int", "foo", 123)))
+  }
+
+  @Test
+  def testEmpty {
+    for (resultType <- ResultOutputType.values) {
+      val env1 = I2b2ResultEnvelope.empty(resultType)
+
+      env1 should not be (null)
+      env1.resultType should be(resultType)
+      env1.columns.isEmpty should be(true)
+
+      val env2 = I2b2ResultEnvelope.empty(resultType)
+
+      (env1 eq env2) should be(false)
+    }
+  }
+}
