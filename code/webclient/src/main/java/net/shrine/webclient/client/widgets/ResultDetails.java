6ac36315fe43f82e192462f29a20b9098abcdc30
cgilbert <cgilbert@c2d9aaf4-0be0-4a0d-89e0-85a3af7b581e>
2012-09-20 18:38:47 +0000

    ResultDetails now just a Java class, with no ui.xml counterpart

--- code/webclient/src/main/java/net/shrine/webclient/client/widgets/ResultDetails.java
+++ code/webclient/src/main/java/net/shrine/webclient/client/widgets/ResultDetails.java
@@ -1,71 +1,63 @@
 package net.shrine.webclient.client.widgets;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Composite;
+import java.util.List;
+import java.util.Map;
+
+import net.shrine.webclient.client.util.Util;
+import net.shrine.webclient.shared.domain.Breakdown;
+import net.shrine.webclient.shared.domain.SingleInstitutionQueryResult;
+
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.visualization.client.AbstractDataTable;
 import com.google.gwt.visualization.client.DataTable;
 import com.google.gwt.visualization.client.visualizations.corechart.AxisOptions;
 import com.google.gwt.visualization.client.visualizations.corechart.ColumnChart;
 import com.google.gwt.visualization.client.visualizations.corechart.CoreChart;
 import com.google.gwt.visualization.client.visualizations.corechart.Options;
-import net.shrine.webclient.shared.domain.Breakdown;
-import net.shrine.webclient.shared.domain.SingleInstitutionQueryResult;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author Bill Simons
  * @date 9/19/12
  * @link http://cbmi.med.harvard.edu
  * @link http://chip.org
- * <p/>
- * NOTICE: This software comes with NO guarantees whatsoever and is
- * licensed as Lgpl Open Source
+ *       <p/>
+ *       NOTICE: This software comes with NO guarantees whatsoever and is
+ *       licensed as Lgpl Open Source
  * @link http://www.gnu.org/licenses/lgpl.html
  */
-public class ResultDetails extends Composite {
-    interface ResultDetailsUiBinder extends UiBinder<HTMLPanel, ResultDetails> {
-    }
-
-    private static ResultDetailsUiBinder uiBinder = GWT.create(ResultDetailsUiBinder.class);
-
-    @UiField
-    FlowPanel delegate;
+public final class ResultDetails extends FlowPanel {
 
     public ResultDetails(final SingleInstitutionQueryResult result) {
-        initWidget(uiBinder.createAndBindUi(this));
-        for(Map.Entry<String, Breakdown> entry : result.getBreakdowns().entrySet()) {
-            DataTable dataTable = getDataTable(entry.getValue());
-            Options options = CoreChart.createOptions();
+        super();
+        
+        this.setVisible(false);
+        
+        for (final Map.Entry<String, Breakdown> entry : result.getBreakdowns().entrySet()) {
+            final DataTable dataTable = getDataTable(entry.getValue());
+            final Options options = CoreChart.createOptions();
             options.setHeight(167);
             options.setTitle(entry.getKey());
             options.setWidth(465);
 
-            AxisOptions vAxisOptions = AxisOptions.create();
+            final AxisOptions vAxisOptions = AxisOptions.create();
             vAxisOptions.setMinValue(0);
             options.setVAxisOptions(vAxisOptions);
-            ColumnChart chart = new ColumnChart(dataTable, options);
-            delegate.add(chart);
+            
+            this.add(new ColumnChart(dataTable, options));
         }
-        delegate.setVisible(true);
+        
+        this.setVisible(true);
     }
 
-
-    private static DataTable getDataTable(Breakdown value) {
-        DataTable data = DataTable.create();
+    private static DataTable getDataTable(final Breakdown value) {
+        final DataTable data = DataTable.create();
         data.addColumn(AbstractDataTable.ColumnType.STRING, "Demographics Category");
         data.addRows(1);
         data.setValue(0, 0, "# of Patients");
-        ArrayList<Map.Entry<String, Long>> entries = new ArrayList<Map.Entry<String, Long>>(value.entrySet());
-        for(int i = 0; i < entries.size(); i++) {
+        final List<Map.Entry<String, Long>> entries = Util.makeArrayList(value.entrySet());
+        for (int i = 0; i < entries.size(); i++) {
             data.addColumn(AbstractDataTable.ColumnType.NUMBER, entries.get(i).getKey());
-            data.setValue(0, i+1, entries.get(i).getValue());
+            data.setValue(0, i + 1, entries.get(i).getValue());
         }
         return data;
     }
