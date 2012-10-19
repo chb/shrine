0b4e34ffbd6d87253e6db52807f6eeea336e9584
cgilbert <cgilbert@c2d9aaf4-0be0-4a0d-89e0-85a3af7b581e>
2012-10-19 17:57:20 +0000

    Table names changed to upper case to match Oracle's expectations. :(

--- code/adapter/src/main/resources/adapter.sql
+++ code/adapter/src/main/resources/adapter.sql
@@ -1,4 +1,6 @@
-create table shrine_query(
+use slicktest;
+
+create table SHRINE_QUERY(
   id int not null auto_increment,
   network_id bigint not null,
   username varchar(255) not null,
@@ -8,59 +10,59 @@ create table shrine_query(
   constraint query_id_pk primary key(id)
 ) engine=innodb default charset=latin1;
 
-create table query_result(
+create table QUERY_RESULT(
   id int not null auto_increment,
   query_id int not null,
   type enum('PATIENTSET','PATIENT_COUNT_XML','PATIENT_AGE_COUNT_XML','PATIENT_RACE_COUNT_XML','PATIENT_VITALSTATUS_COUNT_XML','PATIENT_GENDER_COUNT_XML','ERROR') not null,
   status enum('FINISHED', 'ERROR', 'PROCESSING', 'QUEUED') not null,
   time_elapsed int not null,
   last_updated timestamp default current_timestamp,
-  constraint query_result_id_pk primary key(id),
-  constraint fk_query_result_query_id foreign key (query_id) references shrine_query (id)
+  constraint QUERY_RESULT_id_pk primary key(id),
+  constraint fk_QUERY_RESULT_query_id foreign key (query_id) references SHRINE_QUERY (id)
 ) engine=innodb default charset=latin1;
 
-create table error_result(
+create table ERROR_RESULT(
   id int not null auto_increment,
   result_id int not null,
   message varchar(255) not null,
-  constraint error_result_id_pk primary key(id),
-  constraint fk_error_result_query_result_id foreign key (result_id) references query_result (id)
+  constraint ERROR_RESULT_id_pk primary key(id),
+  constraint fk_ERROR_RESULT_QUERY_RESULT_id foreign key (result_id) references QUERY_RESULT (id)
 ) engine=innodb default charset=latin1;
 
-create table count_result(
+create table COUNT_RESULT(
   id int not null auto_increment,
   result_id int not null,
   original_count int not null,
   obfuscated_count int not null,
   date_created timestamp default current_timestamp,
-  constraint count_result_id_pk primary key(id),
-  constraint fk_count_result_query_result_id foreign key (result_id) references query_result (id)
+  constraint COUNT_RESULT_id_pk primary key(id),
+  constraint fk_COUNT_RESULT_QUERY_RESULT_id foreign key (result_id) references QUERY_RESULT (id)
 ) engine=innodb default charset=latin1;
 
-create table breakdown_result(
+create table BREAKDOWN_RESULT(
   id int not null auto_increment,
   result_id int not null,
   data_key varchar(255) not null,
   original_value int not null,
   obfuscated_value int not null,
-  constraint breakdown_result_id_pk primary key(id),
-  constraint fk_breakdown_result_query_result_id foreign key (result_id) references query_result (id)
+  constraint BREAKDOWN_RESULT_id_pk primary key(id),
+  constraint fk_BREAKDOWN_RESULT_QUERY_RESULT_id foreign key (result_id) references QUERY_RESULT (id)
 ) engine=innodb default charset=latin1;
 
-create table patient_set(
+create table PATIENT_SET(
   id int not null auto_increment,
   result_id int not null,
   patient_num varchar(255) not null,
-  constraint patient_set_id_pk primary key(id),
-  constraint fk_patient_set_query_result_id foreign key (result_id) references query_result (id)
+  constraint PATIENT_SET_id_pk primary key(id),
+  constraint fk_PATIENT_SET_QUERY_RESULT_id foreign key (result_id) references QUERY_RESULT (id)
 ) engine=innodb default charset=latin1;
 
-create table privileged_user(
+create table PRIVILEGED_USER(
   id int not null auto_increment,
   username varchar(255) not null,
   domain varchar(255) not null,
   threshold int not null,
   override_date timestamp null,
   constraint priviliged_user_pk primary key(id),
-  constraint ix_privileged_user_username_domain unique (username, domain)
+  constraint ix_PRIVILEGED_USER_username_domain unique (username, domain)
 ) engine=innodb default charset=latin1;
