d7f6ff5e8efc9c4ef57f158bc8033db30bde5e3c
cgilbert <cgilbert@c2d9aaf4-0be0-4a0d-89e0-85a3af7b581e>
2012-12-07 00:12:15 +0000

    Add mysql adapter db drop script

---
+++ code/adapter/src/main/resources/adapter-drop.sql
@@ -0,0 +1,7 @@
+drop table PRIVILEGED_USER;
+drop table ERROR_RESULT;
+drop table COUNT_RESULT;
+drop table BREAKDOWN_RESULT;
+drop table PATIENT_SET;
+drop table QUERY_RESULT;
+drop table SHRINE_QUERY;
