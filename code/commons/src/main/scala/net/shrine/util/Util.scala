8d979328f5a10c1b0f2715a8da1e703baa43f897
cgilbert <cgilbert@c2d9aaf4-0be0-4a0d-89e0-85a3af7b581e>
2013-02-04 19:49:59 +0000

    Added comments

--- code/commons/src/main/scala/net/shrine/util/Util.scala
+++ code/commons/src/main/scala/net/shrine/util/Util.scala
@@ -39,11 +39,22 @@ object Util extends Loggable {
       implicit def try2Option[T](o: Try[T]): Option[T] = o.toOption
     }
 
+    /**
+     * Turns an Option[Try[T]] into a Try[Option[T]], a la Future.sequence.
+     */
     def sequence[T](tryOption: Option[Try[T]]): Try[Option[T]] = tryOption match {
       case Some(attempt) => attempt.map(Option(_))
       case None => Try(None)
     }
 
+    /**
+     * Turns an Option[Traversable[T]] into a Try[Traversable[T]], a la Future.sequence.
+     * Uses CanBuildFrom bagic to ensure that the subtype of Traversable passed in is the
+     * same subtype of Traversable returned, and that this is verifiable at compile-time.
+     * 
+     * NB: If *any* of the input Tries are Failures, then the first Failure is returned;
+     * this can drop subsequent Failures if there are more than one.
+     */
     def sequence[A, C[+A] <: Traversable[A]](tries: C[Try[A]])(implicit cbf: CanBuildFrom[C[A], A, C[A]]): Try[C[A]] = {
       val firstFailure: Option[Failure[A]] = tries.find(_.isFailure).collect { case f: Failure[A] => f }
 
