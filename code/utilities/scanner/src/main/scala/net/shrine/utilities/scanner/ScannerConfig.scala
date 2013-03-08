18c102743eece9af1bc0f9c2eceb8bcde5874bf7
cgilbert <cgilbert@c2d9aaf4-0be0-4a0d-89e0-85a3af7b581e>
2013-03-08 21:22:29 +0000

    Use Typesafe Config for the Scanner util

--- code/utilities/scanner/src/main/scala/net/shrine/utilities/scanner/ScannerConfig.scala
+++ code/utilities/scanner/src/main/scala/net/shrine/utilities/scanner/ScannerConfig.scala
@@ -2,15 +2,70 @@ package net.shrine.utilities.scanner
 
 import scala.concurrent.duration.Duration
 import net.shrine.protocol.AuthenticationInfo
+import net.shrine.protocol.Credential
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import com.typesafe.config.ConfigException
 
 /**
  * @author clint
  * @date Mar 6, 2013
  */
 final case class ScannerConfig(
-    val adapterMappingsFile: String, 
-    val ontologySqlFile: String, 
-    val reScanTimeout: Duration,
-    val shrineUrl: String, 
-    val projectId: String, 
-    val authorization: AuthenticationInfo)
+  val adapterMappingsFile: String,
+  val ontologySqlFile: String,
+  val reScanTimeout: Duration,
+  val shrineUrl: String,
+  val projectId: String,
+  val authorization: AuthenticationInfo)
+
+object ScannerConfig {
+  private[scanner] def getReScanTimeout(subConfig: Config): Duration = {
+    import scala.concurrent.duration._
+    import Keys._
+    
+    if (subConfig.hasPath(milliseconds)) { subConfig.getLong(milliseconds).milliseconds }
+    else if (subConfig.hasPath(seconds)) { subConfig.getLong(seconds).seconds }
+    else if (subConfig.hasPath(minutes)) { subConfig.getLong(minutes).minutes }
+    else { throw new ConfigException.Missing(s"Expected to find one of scanner.reScanTimeout.{${ milliseconds }, ${ seconds }, ${ minutes }} at subConfig") }
+  }
+
+  def apply(config: Config): ScannerConfig = {
+    import Keys._
+    
+    def getAuthInfo(subConfig: Config): AuthenticationInfo = {
+      require(subConfig.hasPath(domain))
+      require(subConfig.hasPath(username))
+      require(subConfig.hasPath(password))
+
+      AuthenticationInfo(subConfig.getString(domain), subConfig.getString(username), Credential(subConfig.getString(password), false))
+    }
+
+    ScannerConfig(
+      config.getString(adapterMappingsFile),
+      config.getString(ontologySqlFile),
+      getReScanTimeout(config.getConfig(reScanTimeout)),
+      config.getString(shrineUrl),
+      config.getString(projectId),
+      getAuthInfo(config.getConfig(credentials)))
+  }
+
+  object Keys {
+    val minutes = "minutes"
+    val seconds = "seconds"
+    val milliseconds = "milliseconds"
+
+    val domain = "domain"
+    val username = "username"
+    val password = "password"
+
+    private def subKey(k: String) = s"scanner.$k"
+
+    val adapterMappingsFile = subKey("adapterMappingsFile")
+    val ontologySqlFile = subKey("ontologySqlFile")
+    val reScanTimeout = subKey("reScanTimeout")
+    val shrineUrl = subKey("shrineUrl")
+    val projectId= subKey("projectId")
+    val credentials = subKey("credentials")
+  }
+}
---
+++ code/utilities/scanner/src/test/scala/net/shrine/utilities/scanner/ScannerConfigTest.scala
@@ -0,0 +1,56 @@
+package net.shrine.utilities.scanner
+
+import junit.framework.TestCase
+import org.scalatest.junit.ShouldMatchersForJUnit
+import org.junit.Test
+import com.typesafe.config.ConfigFactory
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigException
+
+/**
+ * @author clint
+ * @date Mar 8, 2013
+ */
+final class ScannerConfigTest extends TestCase with ShouldMatchersForJUnit {
+  import scala.concurrent.duration._
+
+  @Test
+  def testFromConfig {
+    val scannerConfig = ScannerConfig(ConfigFactory.load())
+
+    scannerConfig should not be (null)
+    scannerConfig.adapterMappingsFile should be("testAdapterMappings.xml")
+    scannerConfig.ontologySqlFile should be("testShrineWithSyns.sql")
+    scannerConfig.reScanTimeout should be(99.seconds)
+    scannerConfig.shrineUrl should be("https://example.com")
+    scannerConfig.projectId should be("SHRINE-PROJECT")
+    scannerConfig.authorization should not be (null)
+    scannerConfig.authorization.domain should be("TestDomain")
+    scannerConfig.authorization.username should be("testuser")
+    scannerConfig.authorization.credential.value should be("testpassword")
+    scannerConfig.authorization.credential.isToken should be(false)
+  }
+
+  @Test
+  def testGetReScanTimeout {
+    intercept[ConfigException.Missing] {
+      ScannerConfig.getReScanTimeout(ConfigFactory.empty())
+    }
+    
+    import scala.collection.JavaConverters._
+    
+    import ScannerConfig.Keys.{milliseconds, seconds, minutes}
+    
+    ScannerConfig.getReScanTimeout(ConfigFactory.parseMap(Map(milliseconds -> "123").asJava)) should be(123.milliseconds)
+    
+    ScannerConfig.getReScanTimeout(ConfigFactory.parseMap(Map(seconds -> "123").asJava)) should be(123.seconds)
+    
+    ScannerConfig.getReScanTimeout(ConfigFactory.parseMap(Map(minutes -> "123").asJava)) should be(123.minutes)
+    
+    //Priority should be millis > seconds > minutes
+    
+    ScannerConfig.getReScanTimeout(ConfigFactory.parseMap(Map(milliseconds -> "123", seconds -> "456", minutes -> "789" ).asJava)) should be(123.milliseconds)
+    
+    ScannerConfig.getReScanTimeout(ConfigFactory.parseMap(Map(seconds -> "456", minutes -> "789" ).asJava)) should be(456.seconds)
+  }
+}
