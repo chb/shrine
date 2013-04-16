5ee444c7e0a75ae3ff1a7a4803b06e29e89c84ad
cgilbert <cgilbert@c2d9aaf4-0be0-4a0d-89e0-85a3af7b581e>
2013-04-16 17:40:24 +0000

    Added exception-logging versions of all logging methods in Loggable, not just error()

--- code/commons/src/main/scala/net/shrine/util/Loggable.scala
+++ code/commons/src/main/scala/net/shrine/util/Loggable.scala
@@ -17,8 +17,14 @@ trait Loggable {
   lazy val infoEnabled = internaLogger.isInfoEnabled
   
   def debug(s: => Any): Unit = if(internaLogger.isDebugEnabled) internaLogger.debug(s)
+  def debug(s: => Any, e: Throwable): Unit = if(internaLogger.isDebugEnabled) internaLogger.debug(s, e)
+  
   def info(s: => Any): Unit = if(internaLogger.isInfoEnabled) internaLogger.info(s)
+  def info(s: => Any, e: Throwable): Unit = if(internaLogger.isInfoEnabled) internaLogger.info(s, e)
+  
   def warn(s: => Any): Unit = internaLogger.warn(s)
+  def warn(s: => Any, e: Throwable): Unit = internaLogger.warn(s, e)
+  
   def error(s: => Any): Unit = internaLogger.error(s)
   def error(s: => Any, e: Throwable): Unit = internaLogger.error(s, e)
 }
