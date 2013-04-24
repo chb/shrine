62106af26ad5763c972f3bde8f0aacd12bebce89
cgilbert <cgilbert@c2d9aaf4-0be0-4a0d-89e0-85a3af7b581e>
2013-04-24 21:53:01 +0000

    Pre-release cleanup: removed ont-service and webclient modules

--- code/webclient/src/main/java/net/shrine/webclient/client/util/IObservable.java
+++
@@ -1,9 +0,0 @@
-package net.shrine.webclient.client.util;
-
-public interface IObservable {
-  void observedBy(final Observer observer);
-  
-  void noLongerObservedBy(final Observer observer);
-  
-  void notifyObservers();
-}
--- code/webclient/src/main/scala/net/shrine/webclient/server/api/BootstrapInfoMessageBodyWriter.scala
+++
@@ -1,7 +0,0 @@
-package net.shrine.webclient.server.api
-
-import javax.ws.rs.ext.Provider
-import net.shrine.webclient.shared.domain.BootstrapInfo
-
-@Provider
-final class BootstrapInfoMessageBodyWriter extends SingleObjectJsonMessageBodyWriter[BootstrapInfo]
