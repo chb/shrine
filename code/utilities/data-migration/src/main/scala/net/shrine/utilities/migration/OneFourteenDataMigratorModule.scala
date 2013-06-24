89b4c7479d4b2b16f80222846fba9cf539975987
bsimons <bsimons@c2d9aaf4-0be0-4a0d-89e0-85a3af7b581e>
2013-06-24 15:26:12 +0000

    accept prop file configuration for migration tool

--- code/utilities/data-migration/src/main/scala/net/shrine/utilities/migration/OneFourteenDataMigratorModule.scala
+++ code/utilities/data-migration/src/main/scala/net/shrine/utilities/migration/OneFourteenDataMigratorModule.scala
@@ -1,7 +1,7 @@
 package net.shrine.utilities.migration
 
 import org.squeryl.Schema
-import org.squeryl.adapters.MySQLAdapter
+import org.squeryl.adapters.{MSSQLServer, OracleAdapter, MySQLAdapter}
 
 import net.shrine.adapter.dao.squeryl.SquerylEntryPoint
 import net.shrine.adapter.dao.squeryl.tables.BreakdownResultsComponent
@@ -12,19 +12,44 @@ import net.shrine.adapter.dao.squeryl.tables.ShrineQueriesComponent
 import net.shrine.dao.squeryl.JdbcUrlSquerylInitializer
 import net.shrine.dao.squeryl.SquerylInitializer
 import net.shrine.protocol.query.QueryDefinition
+import com.typesafe.config.{Config, ConfigFactory}
+import org.squeryl.internals.DatabaseAdapter
 
 /**
  * @author clint
  * @date May 3, 2013
  */
 object OneFourteenDataMigratorModule extends App {
-  //Can use OracleDriver, SqlServerDriver, etc
+
+
+  def loadConfig: Config = {
+    //  The convenience method ConfigFactory.load() loads the following (first-listed are higher priority):
+    //
+    //  system properties
+    //  application.conf (all resources on classpath with this name)
+    //  application.json (all resources on classpath with this name)
+    //  application.properties (all resources on classpath with this name)
+    //  reference.conf (all resources on classpath with this name)
+    ConfigFactory.load()
+  }
+
+  def identifyAdapter(adapterName: String): DatabaseAdapter =
+    adapterName match {
+      case "mysql" => new MySQLAdapter
+      case "oracle" => new OracleAdapter
+      case "sqlserver" => new MSSQLServer
+      case _ => throw new IllegalArgumentException("db-adapter must be one of msyql, oracle, sqlserver")
+    }
+
+
+  val config = loadConfig
+
   val initializer: SquerylInitializer = new JdbcUrlSquerylInitializer(
-  										  new MySQLAdapter, 
-    									  """jdbc driver class name""", 
-    									  """jdbc url""", 
-    									  """jdbc user""", 
-    									  """jdbc password""")
+    identifyAdapter(config.getString("db-adapter")),
+    config.getString("driver-name"),
+    config.getString("jdbc-url"),
+    config.getString("jdbc-user"),
+    config.getString("jdbc-password"))
 
   val module = new OneFourteenDataMigratorModule(initializer)
 
@@ -37,10 +62,10 @@ import SquerylEntryPoint._
  * @author clint
  * @date May 3, 2013
  */
-final class OneFourteenDataMigratorModule(initializer: SquerylInitializer) extends 
+final class OneFourteenDataMigratorModule(initializer: SquerylInitializer) extends
 	Schema with
-	CountResultsComponent with 
-	BreakdownResultsComponent with 
+	CountResultsComponent with
+	BreakdownResultsComponent with
 	ErrorResultsComponent with
 	QueryResultsComponent with
 	ShrineQueriesComponent {
@@ -51,13 +76,13 @@ final class OneFourteenDataMigratorModule(initializer: SquerylInitializer) exten
   //row in the SHRINE_QUERY table.
   def migrate() {
     initializer.init
-    
+
     inTransaction {
       val i2b2FormatQueries = from(shrineQueries)(select(_))
-      
+
       i2b2FormatQueries.iterator.foreach { i2b2FormatQuery =>
         val shrineFormatExpr = QueryDefinition.fromI2b2(i2b2FormatQuery.queryExpr).get.expr.toXmlString
-        
+
         update(shrineQueries) { shrineQueryForUpdate =>
           where(i2b2FormatQuery.id === shrineQueryForUpdate.id).
           set(shrineQueryForUpdate.queryExpr := shrineFormatExpr)
